217d46d5e8f36ee8e036dd258698f7bc6eb33a13
natsuki.i <natsuki.iida0511@gmail.com>
2023-09-05 12:23:39 +0200

    Made a small change

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header';
+import App from './App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Header/>  
+    <App/>
   </React.StrictMode>
 );
 
