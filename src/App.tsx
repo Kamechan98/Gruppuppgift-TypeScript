f6a59595712e38b6a4b0e4256f3ab990a5b42f85
Israa <israahasibismail@gmail.com>
2023-09-07 12:24:21 +0200

    detalj

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ThreadListView from "./pages/ThreadListView"
 import Post from './components/Post';
 import PostList from './components/PostList';
+import ThreadDetailView from './pages/ThreadDetailView';
 
 const App = () => {
  return(
@@ -10,6 +11,8 @@ const App = () => {
           {/* <Route path="/" element={<Home />} /> */}
           <Route path="/post" element={<Post />} />
           <Route index element={<PostList />} />
+          <Route path="/post/:id" index element={<ThreadDetailView/>} />
+
         </Routes>
       </Router>
  );   
--- src/pages/ThreadDetailView.tsx
+++ src/pages/ThreadDetailView.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-//Visa detaljer av specifik tråd
-const ThreadDetailView = () => {
-  return (
-    <div>ThreadDetailView</div>
-  )
-}
+// PostDetail.tsx
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+// import { posts } from './data';
 
-export default ThreadDetailView
+
+
+const ThreadDetailView = () => {
+  const {id}=useParams()
+  const [post, setPost] = useState <PostData> () 
+  useEffect (()=>{
+
+  const storeData = localStorage.getItem(`posts`);
+  if (!!storeData){
+    const dataArray: PostData []  = JSON.parse(storeData) || []
+    const _post = dataArray.find (post=> post.id === Number(id))
+    setPost(_post)
+  }
+
+  },[id]) 
+  
+
+  if (!post) return <div>Posten hittades inte</div>;
+
+  return (
+    <div> key={post.id}
+                <p>Title: {post.title}</p>
+                <p>Description: {post.description}</p>
+                <p>Category: {post.category}</p>
+                <p>Creation Date: {post.creationDate}</p>
+                <p>Creator name: {post.creator.name}</p>
+                <p>Username: {post.creator.userName}</p>   
+    </div>
+  );
+};
+
+export default ThreadDetailView;
