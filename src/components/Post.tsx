2dc180e1ed802aabd7bc8938ccc8fdc15f05eb2e
Evelina Nyman <ina.nyman98@gmail.com>
2023-09-07 10:51:50 +0200

    commit

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import React, { useState, useEffect } from 'react';
 
@@ -173,28 +172,6 @@ const Post: React.FC = () => {
           </div>
         ))} */}
       {/* </div> */}
-=======
-import React from 'react';
-
-type PostProps = {
-  title: string;
-  category: string;
-  creationDate: string;
-  description: string;
-  name: string; // Add name and username props
-  userName: string;
-};
-
-const Post: React.FC<PostProps> = ({ title, description, category, creationDate, name, userName }) => {
-  return (
-    <div className="post">
-      <h2>{title}</h2>
-      <p>Category: {category}</p>
-      <p>Description: {description}</p>
-      <p>Created at: {creationDate}</p>
-      <p>Name: {name}</p> {/* Display the Name */}
-      <p>Username: {userName}</p> {/* Display the Username */}
->>>>>>> 9977228c4d4b2a6b30d037253ca4111e7ac02022
     </div>
   );
 };
